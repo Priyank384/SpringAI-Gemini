bda5b6cd42150eadd9549aa53aae4b16adf54866
Priyank384 <priyankgupta384@gmail.com>
2026-08-19 01:04:44 +0530

    Added GuardRails

---
+++ src/main/java/com/example/SpringAIStarter/controllers/GuardRailController.java
@@ -0,0 +1,34 @@
+package com.example.SpringAIStarter.controllers;
+
+import java.lang.module.ModuleDescriptor.Builder;
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/guardrail")
+public class GuardRailController {
+    private final ChatClient chatClient;
+    public GuardRailController(ChatClient.Builder builder){
+        this.chatClient = builder
+                            .defaultSystem("You are a helpful and safe AI assistant")
+                            .defaultAdvisors(SafeGuardAdvisor.builder()
+                                                .sensitiveWords(List.of("Kill","hack","violence","exploit","weapons","attack"))
+                                                .build())
+                            .build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam String message){
+        String response = chatClient.prompt()
+                            .user(message)
+                            .call()
+                            .content();
+        return response;
+    }
+}
